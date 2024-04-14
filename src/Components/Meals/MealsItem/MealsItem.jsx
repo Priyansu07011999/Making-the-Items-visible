2cc6c435aad31da2c459604517f4b96921b39457
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-14 19:48:22 +0530

    Add files via upload

---
+++ src/Components/Meals/MealsItem/MealsItem.jsx
@@ -0,0 +1,18 @@
+import classes from './MealsItem.module.css';
+
+const MealItem = (props) => {
+  const price = `₹${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3 className='text-xl font-bold'>{props.title}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
